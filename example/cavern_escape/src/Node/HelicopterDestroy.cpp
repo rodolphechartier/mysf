4296755f4c396430566e38b8e7ca93c9baed5a03
chartier <chartier@debian>
2017-12-08 13:20:38 +0100

    speed anim modif

--- example/cavern_escape/src/Node/HelicopterDestroy.cpp
+++ example/cavern_escape/src/Node/HelicopterDestroy.cpp
@@ -1,7 +1,7 @@
 #include "HelicopterDestroy.hpp"
 
 HelicopterDestroy::HelicopterDestroy(const mysf::TextureHolder & thl)
-	: AnimNode(thl[Resource::Texture::Helicopter], sf::seconds(0.2f), true, false)
+	: AnimNode(thl[Resource::Texture::Helicopter], sf::seconds(0.135f), true, false)
 {
 	for (unsigned int i = 0; i < 7; ++i)
 		addFrame(sf::IntRect(i * 180, 190, 180, 95));
--- example/cavern_escape/src/Node/HelicopterIdle.cpp
+++ example/cavern_escape/src/Node/HelicopterIdle.cpp
@@ -1,7 +1,7 @@
 #include "HelicopterIdle.hpp"
 
 HelicopterIdle::HelicopterIdle(const mysf::TextureHolder & thl)
-	: AnimNode(thl[Resource::Texture::Helicopter])
+	: AnimNode(thl[Resource::Texture::Helicopter], sf::seconds(0.135f))
 {
 	for (unsigned int i = 0; i < 7; ++i)
 		addFrame(sf::IntRect(i * 180, 0, 180, 95));
